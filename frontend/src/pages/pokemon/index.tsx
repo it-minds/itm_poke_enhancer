fe6731394e3b3d42d08056cfc79ac6c9461dadca
Johnny <jon@it-minds.dk>
2022-02-10 18:47:09 +0100

    Changed link to use next/Link instead

--- frontend/src/pages/pokemon/index.tsx
+++ frontend/src/pages/pokemon/index.tsx
@@ -1,4 +1,5 @@
-import { Box, Link } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import Link from 'next/link'
 import { NextPage } from "next";
 import React, { useEffect, useState } from "react";
 import { client } from "services/backend/client";
@@ -19,7 +20,7 @@ const PokelistPage: NextPage = () => {
   return (
     <div>
       {basePokemon.map(pm => (
-        <Link key={pm.id} href={`/pokemon/${pm.id}`}>
+        <Link key={pm.id} href={{ pathname: `/pokemon/${pm.id}`, query: { id: pm.id } }} >
           <Box bg="tomato" w="100%" p={4} color="white">
             {pm.id} - {pm.name}
           </Box>
